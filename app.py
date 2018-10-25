ebb7290dac048422b797f21fa962491703b65897
aboud20-meet <aboud20@meet.mit.edu>
2018-10-25 18:47:27 +0300

    started web app

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-    return None
+    return render_template("home.html")
+
+@app.route('/shop')
+def shop_page():
+    phones = ["Samsung Galaxy 8", "Samsung Galaxy 9", "Samsung Galaxy 7+", "iPhone 7"] 
+    return render_template("shop.html", phones=phones)
 
 if __name__ == '__main__':
    app.run(debug = True)
